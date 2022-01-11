5ac2f4f4a1160559626f4797d002d7bd1ceba72a
subham kumar <subhamkumarchandrawansi@gmail.com>
2022-01-11 07:19:10 +0530

    aability to add multiple cells, enter key creates new cell

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { BlogCell } from './components/blog-cell/blog-cell';
+import { useState } from 'react';
 
 function App() {
+  const [cells, setCells] = useState([''])
+  let cellFocused = {}
+
+  const addNewBlogCell = () => {
+    setCells([...cells, ''])
+    cellFocused[`cell-${cells.length - 1}`].focus()
+  }
+
   return (
     <div className="App">
-      <BlogCell />
+      {cells.map((e, i) => <BlogCell key={i} addCell={addNewBlogCell} ref={div =>  cellFocused[`cell-${i}`] = div} />)}
+      <button onClick={addNewBlogCell}>Add New Cell</button>
     </div>
   );
 }
--- src/components/blog-cell/blog-cell.js
+++ src/components/blog-cell/blog-cell.js
@@ -1,26 +1,41 @@
 import { useState } from "react"
 import React from 'react';
 
-export const BlogCell = () => {
+export const BlogCell = ({addCell}) => {
 
-  const formats = {
-    'p' : 'p',
-    'h1' : 'h1'
+  // const formats = {
+  //   'p' : 'p',
+  //   'h1' : 'h1'
+  // }
+
+  // const [currentFormat, setCurrentFormatting] = useState(formats.p)
+
+  const [value, setValue] = useState({html: '', text: ''})
+
+  const _changeListener = (event) => {
+    // console.log(event)
+    const [text, html] = [event.target.innerText, event.target.innerHTML]
+    setValue({text: text, html: html})
   }
 
-  const [currentFormat, setCurrentFormatting] = useState(formats.p)
-
-  const _changeListener = (editorState) => {
-    console.log(editorState)
+  const _handleKeyDown = (event) => {
+    if (event.key === 'Enter')
+    {
+        addCell()
+    }
+    console.log(event)
   }
 
   return (<div>
-    <p>Current Format {currentFormat}</p>
+    {/* <p>Current Format {currentFormat}</p>
     <button onClick={() => setCurrentFormatting(formats.h1)}>h1</button>
-    <button onClick={() => setCurrentFormatting(formats.p)}>p</button>
+    <button onClick={() => setCurrentFormatting(formats.p)}>p</button> */}
+
+    <p>{value.text}</p>
+    <p>{value.html}</p>
     <br />
     <br />
-    <div onInput={(event) => _changeListener(event)} contentEditable="true" className='blog-cell' >{}</div>
+    <div onInput={(event) => _changeListener(event)} contentEditable="true" className='blog-cell' onKeyDown={_handleKeyDown} ></div>
  
   </div>)
 }
